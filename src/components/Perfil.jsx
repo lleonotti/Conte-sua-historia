45a39d92d5f53855dc80c6c36efeed159226cbf4
Leonardo Leonotti <leonardo.leonotti@conpec.com.br>
2022-02-25 11:52:18 -0300

    Perfil.css

--- src/components/Perfil.jsx
+++ src/components/Perfil.jsx
@@ -1,22 +1,21 @@
-import React, { useState } from 'react';
+import React, { useState } from "react";
 import milos from "../assets/milos.jpg";
-import quit from "../assets/quit-icon.svg"
-import lapiz from "../assets/lapiz.svg"
-import "./Perfil.css"
+import quit from "../assets/quit-icon.svg";
+import lapiz from "../assets/lapiz.svg";
+import "./Perfil.css";
 
 function Perfil() {
-    const [disable, setDisabled] = useState(true)
-    const [name, setName] = useState('Strogonoff')
+  const [disable, setDisabled] = useState(true);
+  const [name, setName] = useState("Strogonoff");
 
-    const enableEditing = () => {
-        setDisabled(false)
-    }
+  const enableEditing = () => {
+    setDisabled(false);
+  };
 
   return (
-  <div className="postAndtags-container">
+    <div className="postAndtags-container">
       <section className="tags-container">
-        <div className="tags-left">
-        </div>
+        <div className="tags-left"></div>
         <img id="button-quit-post" src={quit} alt="Botao sair" />
       </section>
       <div className="post-container">
@@ -29,32 +28,46 @@ function Perfil() {
             />
             <p className="post-username">@ Ricardo Milos</p>
           </div>
-      
         </header>
-        <form className="post-content-container">
-            <section className="top-section-container">
-                <div className="form-left-container">
-                    <div className="form-g">
-                    <label>Endereço:</label><br />
-                    <div className="buttonInside">
-                        <input type="text" id="endereco" disabled={disable} value={""} onChange={e => setName(e.target.value)} />
-                        <button id="editNome" className="buttonInput" type="button" onClick={enableEditing}> <img src={lapiz} alt="" /> </button>
-                    </div>
-            </div>
+        <form className="perfil-content-container">
+          <section className="top-section-container">
+            <div className="form-left-container">
+              <div className="form-g">
+                <label>Endereço:</label>
+                <br />
+                <div className="buttonInside">
+                  <input
+                    type="text"
+                    id="endereco"
+                    disabled={disable}
+                    value={""}
+                    onChange={(e) => setName(e.target.value)}
+                  />
+                  <button
+                    id="editNome"
+                    className="buttonInput"
+                    type="button"
+                    onClick={enableEditing}
+                  >
+                    {" "}
+                    <img src={lapiz} alt="" />{" "}
+                  </button>
                 </div>
-            </section>
-          <div className="form-g">
-                    <label>Endereço:</label><br />
-                    <div className="buttonInside">
-                        <input type="text" id="endereco" disabled={disable} value={""} onChange={e => setName(e.target.value)} />
-                        <button id="editNome" className="buttonInput" type="button" onClick={enableEditing}> <img src={lapiz} alt="" /> </button>
-                    </div>
+              </div>
             </div>
-
+            <div className="form-right-container">
+              <div className="form-g">
+                <label>Foto de perfil</label>
+                <br />
+                <img className="large-foto-perfil" src={milos} alt="" />
+                <div className="buttonInside"></div>
+              </div>
+            </div>
+          </section>
         </form>
       </div>
-      </div>
-      );
+    </div>
+  );
 }
 
 export default Perfil;
